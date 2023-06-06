4b992acbe69ab4ecc3acf26628838112a8b320ed
Milos Pujic <m.pujic@levi9.com>
2023-06-06 16:53:18 +0200

    GHA Changes

---
+++ prepare-reports-folder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Move Merged Playwright HTML report folder to reports folder
+mv merged-html-report reports/playwright
+
+# Move Merged Playwright HTML report folder to reports folder                                                                                                                                          (main)
+mv merged-monocart-report reports/monocart
