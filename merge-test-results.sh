e28d7d80aff27d21d73d6c010cf4ad1549c00251
Milos Pujic <m.pujic@levi9.com>
2023-06-06 12:10:31 +0200

    Added merge test-result and add them to monocart report

---
+++ merge-test-results.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Create the test-results folder
+mkdir -p test-results
+
+# Array of folder names
+folders=(
+  "test-results-firefox-1_2"
+  "test-results-firefox-2_2"
+  "test-results-chromium-1_2"
+  "test-results-chromium-2_2"
+  "test-results-webkit-1_2"
+  "test-results-webkit-2_2"
+)
+
+# Iterate over the array and copy contents if the folder exists
+for folder in "${folders[@]}"; do
+  if [ -d "$folder" ]; then
+    cp -r "$folder"/* test-results/
+  fi
+done
